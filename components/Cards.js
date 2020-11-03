6857c155988e84c90749d6bac8cc67af7f09bd70
stiaannel <christiaannel9@gmail.com>
2020-11-03 23:02:13 +0200

    Updated layout/splash

--- components/Cards.js
+++ components/Cards.js
@@ -1,22 +1,31 @@
 import * as React from 'react';
 import { View } from 'react-native';
-import { Card } from 'react-native-paper';
+import { Card, Avatar, Dialog, Portal  } from 'react-native-paper';
 
 export default function Cards(props) {
   return (
     <View style={{ backgroundColor: '#1e1e1e' }}>
-      <Card style={{ margin: 5 }}>
-        <Card.Cover
-          source={{
-            uri:
-              'https://robohash.org/' +
-              props.username + props.id +
-              'jane?set=set5',
-          }}
-        />
+      <Card style={{ margin: 8 }}>
         <Card.Title
-          title={`${props.id}: ${props.name}(${props.username})`}
-          subtitle={props.email}
+          title={`${props.name}`}
+          subtitle={`Username: ${props.username.toLowerCase()}, Email: (${props.email.toLowerCase()})`}
+          left={() => <Avatar.Image size={50} 
+                        source={{
+                          uri:
+                            'https://robohash.org/' +
+                            props.username + props.id +
+                            'jane?set=set5',
+                        }} 
+                      />
+          }
+          onPress={() =>     <Portal>
+      <Dialog visible={visible} onDismiss={hideDialog}>
+        <Dialog.Actions>
+          <Button onPress={() => console.log('Cancel')}>Cancel</Button>
+          <Button onPress={() => console.log('Ok')}>Ok</Button>
+        </Dialog.Actions>
+      </Dialog>
+    </Portal>}
         />
       </Card>
     </View>
